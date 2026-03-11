644795d1e17452a6b633806ba1309a9362dc58e5
avishekkc100 <avishekkc2059@gmail.com>
2026-03-11 14:19:58 +0545

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,98 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true //helps in efficient searching
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String, //cloudinary url
+        required: true,
+
+    },
+    coverimage:{
+        type:String, //cloudinary url
+    },
+    watchHistory:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    refreshedToken:{
+        type:String,
+    }
+
+},{timestamps:true})
+
+
+//bcrypt concept used 
+UserSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt(this.password,10);
+    next();
+})
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password) //provides the result in true or false
+}
+
+//JWT concept used
+UserSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            //payload
+            _id : this._id,
+            username : this.username,
+            email : this.email,
+            fullName : this.fullName 
+        },
+        //secret key
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            //options
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+UserSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            //payload
+            _id : this._id,
+        },
+        //secret key
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            //options
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User",UserSchema)
